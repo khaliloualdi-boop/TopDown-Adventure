ea09a6333dbd171609b63342b06fdda31e44f999
Khalil Oualdi <khalil.oualdi@epfl.ch>
2026-02-26 14:59:37 +0100

    Premier design

--- gameview.py
+++ gameview.py
@@ -2,12 +2,20 @@ from typing import Final
 import arcade
 
 from constants import *
+from textures import *
+
+def grid_to_pixels(i: int) -> int:
+    return i * TILE_SIZE + (TILE_SIZE // 2)
 
 class GameView(arcade.View):
     """Main in-game view."""
 
     world_width: Final[int]
     world_height: Final[int]
+
+    player: Final[arcade.Sprite]
+    wall = arcade.SpriteList(use_spatial_hash = True)
+    ground = arcade.SpriteList(use_spatial_hash = True)
 
     def __init__(self) -> None:
         # Magical incantion: initialize the Arcade view
@@ -20,6 +28,20 @@ class GameView(arcade.View):
         self.world_width = 40 * TILE_SIZE
         self.world_height = 20 * TILE_SIZE
 
+        self.player = arcade.Sprite(
+            TEXTURE_PLAYER_IDLE_DOWN,
+            scale=SCALE, center_x=grid_to_pixels(2), center_y=grid_to_pixels(2)
+        )
+
+        for i in range(20):
+            for j in range(40):
+                self.ground.append(arcade.Sprite(TEXTURE_GRASS, scale = SCALE, center_x = grid_to_pixels(i) , center_y = grid_to_pixels(j)))
+
+        grass_coord = [(3,6),(7,2),(2,10),(3,8)]
+
+        for x, y in grass_coord:
+            self.wall.append(arcade.Sprite(TEXTURE_BUSH, scale = SCALE, center_x = grid_to_pixels(x), center_y = grid_to_pixels(y)))
+
     def on_show_view(self) -> None:
         """Called automatically by 'window.show_view(game_view)' in main.py."""
         # When we show the view, adjust the window's size to our world size.
@@ -31,3 +53,6 @@ class GameView(arcade.View):
     def on_draw(self) -> None:
         """Render the screen."""
         self.clear() # always start with self.clear()
+        self.ground.draw()
+        self.wall.draw()
+        arcade.draw_sprite(self.player)
